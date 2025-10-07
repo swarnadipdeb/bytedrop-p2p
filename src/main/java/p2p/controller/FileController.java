d0860ae75e6ee964d7c2bbff3e79cddceb63ada5
Swarnadip <121611443+swarnadipdeb@users.noreply.github.com>
2025-10-07 17:04:28 +0530

    Update FileController.java(fixed return type)

--- src/main/java/p2p/controller/FileController.java
+++ src/main/java/p2p/controller/FileController.java
@@ -57,10 +57,13 @@ public class FileController {
 
     private class HealthCheck implements HttpHandler {
         @Override
-        public void handle(HttpExchange exchange) throws IOException {
-                String response = "Server Running";
-                return exchange.sendResponseHeaders(200,response.getBytes().length);
+       public void handle(HttpExchange exchange) throws IOException {
+        String response = "Server Running";
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
         }
+      }
     }
     
     private class CORSHandler implements HttpHandler {
